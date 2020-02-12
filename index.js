8650c074a591ad8571442834f26fcc88c360aa5c
Trent Smith <trentnsmith@gmail.com>
2020-02-12 17:42:05 -0600

    updated js to user can input new text

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 "use strict";
 
 
-function getDogImage(textInput) {
-    fetch(`https://dog.ceo/api/breed/${textInput}/images/random`)
+function userInput() {
+  let dogBreed = $("#numDogs").val();
+  return dogBreed;
+}
+
+
+function getDogImage() {
+  fetch("https://dog.ceo/api/breed/" + userInput() + "/images/random")
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson))
     .catch(error => alert("I cannot find that breed. Try again."));
-
 }
 
 function displayResults(responseJson) {
-    console.log(responseJson);
-    
-    $('.results').replaceWith(
-        `<img src="${responseJson.message}" class="results-img">`
-    )
-
-    $('.results').removeClass('hidden');
+  console.log(responseJson);
+  if (responseJson.status !== "success") {
+    alert("I cannot find that breed. Try again.");
+  } else if (responseJson.status === "success") {
+    $(".results").replaceWith(
+      `<img src="${responseJson.message}" class="results">`
+    );
+    $(".results").removeClass("hidden");
+  }
 }
-
 function watchForm() {
-    $('form').submit(event => {
-        event.preventDefault();
-        let userInput = $("#numDogs").val();
-        getDogImage(userInput);
+    $("form").submit(event => {
+      event.preventDefault();
+      getDogImage();
     });
-}
-
+  }
+  
 $(function() {
-    console.log('App loaded! Waiting for submit!');
+    console.log("App loaded! Waiting for submit!");
     watchForm();
-});
+  });
+  
